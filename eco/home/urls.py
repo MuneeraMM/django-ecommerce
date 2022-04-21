eb2177431e25863b693605c505dae291164e5b2f
muneeramm <muneeraabbas22@gmail.com>
2022-04-21 08:22:03 +0530

    registratn

--- eco/home/urls.py
+++ eco/home/urls.py
@@ -2,8 +2,11 @@ from django import views
 from django.contrib import admin
 from django.urls import path
 from . import views
+from home.views import home, contact,LOGINPAGE,RegistrationPage
 
 urlpatterns = [
     path('',views.home,name="home"),
-    path('contact/',views.contact,name="contact")
+    path('contact/',views.contact,name="contact"),
+    path('login/',views.LOGINPAGE,name="login"),
+    path('register/',views.RegistrationPage,name='register')
 ]
--- eco/home/views.py
+++ eco/home/views.py
@@ -1,22 +1,66 @@
+import email
 from multiprocessing import context
 from tempfile import template
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 
 # Create your views here.
 from django.http import HttpResponse
 
+from .form import contactform,LoginForm,RegisterForm
+
+from django.contrib.auth import authenticate,login, get_user_model
+
 def home(request):
+    
     context={
         'title':'homepage',
-        'description':'this is a home page aaaanu'
+        
     }
     return render(request,'home/home.html',context)
 
 def contact(request):
+    form = contactform(request.POST or None)
     context={
         'title':'contactpage',
+        'form':form,
     }
-    if request.method == 'POST':
-        print (request.POST)
-        print(request.POST.get('fullname'))
+    if form.is_valid():
+        print(form.cleaned_data)
+
+    # if request.method == 'POST':
+    #     print (request.POST)
+    #     print(request.POST.get('fullname'))
     return render(request,'contact/contactpage.html',context)
+
+
+def LOGINPAGE(request):
+    form  = LoginForm(request.POST or None)
+    context = {
+        'form':form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        password = form.cleaned_data.get('password')
+        user = authenticate(request, username=username,password=password)
+        if user is not None:
+            print(request.user.is_authenticated)
+            login(request, user)
+            return redirect('/login') 
+        else:
+            print("Error")
+    return render(request,'registration/loginpage.html',context)
+User = get_user_model()
+def RegistrationPage(request):
+    form = RegisterForm(request.POST or None)
+    context = {
+        'form':form
+    }
+    if form.is_valid():
+        print(form.cleaned_data)
+        username = form.cleaned_data.get('username')
+        email = form.cleaned_data.get('email')
+        password = form.cleaned_data.get('password')
+        new_user = User.objects.create_user(username,email,password)
+        print(new_user)
+    return render(request,'registration/registerpage.html',context)
